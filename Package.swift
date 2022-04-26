48abd2406b8f188f900fa442f3ce26e8dc9fa3e9
Ivan O <ioparin@pm.me>
2022-04-26 21:45:46 +0400

    fixing binary artifact versioning

--- Package.swift
+++ Package.swift
@@ -17,17 +17,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TensorFlowLiteC",
-            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0.1/TensorFlowLiteC.xcframework.zip",
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.10/TensorFlowLiteC.xcframework.zip",
             checksum: "86c944d7b108f2f27a637bdb9cc6685ed93f27ee5acbbf1663a12d2da856a014"
         ),
         .binaryTarget(
             name: "TensorFlowLiteCCoreML",
-            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0.1/TensorFlowLiteCCoreML.xcframework.zip",
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.10/TensorFlowLiteCCoreML.xcframework.zip",
             checksum: "651840dc4619804503d5deb288806cfa24e67d311fbec5882bebccffd0c7d376"
         ),
         .binaryTarget(
             name: "TensorFlowLiteCMetal",
-            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0.1/TensorFlowLiteCMetal.xcframework.zip",
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.10/TensorFlowLiteCMetal.xcframework.zip",
             checksum: "857a696d9b3c1e3f53e5db24291bcbf51d5b945f10122661957e31384fa58720"
         )
     ]
