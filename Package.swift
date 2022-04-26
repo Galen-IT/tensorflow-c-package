981b536aacfaac54fdaa247a4c5ba96d2f93fba0
Ivan O <ioparin@pm.me>
2022-04-26 21:29:51 +0400

    updating package to support apple silicone simulator builds

--- Package.swift
+++ Package.swift
@@ -17,18 +17,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TensorFlowLiteC",
-            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0/TensorFlowLiteC.xcframework.zip",
-            checksum: "5f90d1c0b4788b715d9778011d9d513ac22a8bd3d74b7a1fa236ab33242288f7"
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0.1/TensorFlowLiteC.xcframework.zip",
+            checksum: "86c944d7b108f2f27a637bdb9cc6685ed93f27ee5acbbf1663a12d2da856a014"
         ),
         .binaryTarget(
             name: "TensorFlowLiteCCoreML",
-            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0/TensorFlowLiteCCoreML.xcframework.zip",
-            checksum: "931d0a7b03d2f862685ddb52ceaa9f79877d749470193d9520bda209b9e78a0b"
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0.1/TensorFlowLiteCCoreML.xcframework.zip",
+            checksum: "651840dc4619804503d5deb288806cfa24e67d311fbec5882bebccffd0c7d376"
         ),
         .binaryTarget(
             name: "TensorFlowLiteCMetal",
-            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0/TensorFlowLiteCMetal.xcframework.zip",
-            checksum: "7c7be5b84e99bd7a834d998294c7c950c2e838c3bf7cbfaea99a9415c396b86f"
+            url: "https://github.com/galen-it/tensorflow-ios-package/releases/download/2.7.0.1/TensorFlowLiteCMetal.xcframework.zip",
+            checksum: "857a696d9b3c1e3f53e5db24291bcbf51d5b945f10122661957e31384fa58720"
         )
     ]
 )
